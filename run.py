0730d6bae63ab63baf620b96adbfa351cb7c0193
peter <peter@taswira.ai>
2022-12-01 21:27:47 +0300

    requiremnts

--- run.py
+++ run.py
@@ -31,29 +31,13 @@ def index():
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
             # set the file path
             uploaded_file.save(file_path)
-        print(uploaded_file) 
-        print(uploaded_file.filename)   
-        print(file_path)
         df=pd.read_csv(file_path)
         pivot_ui(df, outfile_path='templates/pivottablejs.html')
-        HTML('templates/pivottablejs.html')
+        # HTML('templates/pivottablejs.html')
         return render_template('pivottablejs.html')
     # return 'test'
     return render_template('index.html')
 
-@app.route('/pivottable',methods=["POST"])
-def results():
-    # get the uploaded file
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-        # set the file path
-        uploaded_file.save(file_path)
-    print(uploaded_file)    
-    df=pd.read_csv("media/mps.csv")
-    pivot_ui(df, outfile_path='templates/pivottablejs.html')
-    HTML('templates/pivottablejs.html')
-    return render_template('pivottablejs.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
